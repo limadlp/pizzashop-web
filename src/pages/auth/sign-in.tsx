d91b35ae1fde924631b8110d517a8ebed881ff22
limadlp <dlplima@hotmail.com>
2024-04-18 18:05:03 -0300

    feat: add sign-up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -4,6 +4,8 @@ import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
+import { Link } from "react-router-dom";
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -12,16 +14,37 @@ const signInForm = z.object({
 type SignInForm = z.infer<typeof signInForm>;
 
 export function SignIn() {
-  const { register, handleSubmit, formState: {isSubmitting} } = useForm<SignInForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    try {
+      console.log(data);
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("Enviamos um link de autenticação para seu e-mail", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {
+      toast.error("Credenciais inválidas.");
+    }
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="link" asChild className="absolute right-4 top-8 ">
+          <Link to="/sign-up" >
+            Novo estabelecimento
+          </Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center"></div>
           <h1 className="tracking-t text-2xl font-semibold">Acessar painel</h1>
