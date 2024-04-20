3b43985835e6148b2a4a0528ddce95deb28f2315
limadlp <dlplima@hotmail.com>
2024-04-19 22:48:15 -0300

    feat: toggle theme and drodown menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import {Toaster} from 'sonner';
+import { ThemeProvider } from './components/theme/theme-provider';
 
 export function App() {
   return (
     <HelmetProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
       <Helmet titleTemplate='%s | pizza.shop'/>
       <Toaster richColors closeButton />
       <RouterProvider router={router}/>
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
