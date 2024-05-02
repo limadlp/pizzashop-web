35addf35f6110f7c0a4f85446c8d397ebbf32c20
limadlp <dlplima@hotmail.com>
2024-05-02 19:50:26 -0300

    feat: filter orders

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,28 +1,39 @@
 import { api } from "@/lib/axios";
 
-export interface GetOrdersQuery{
+export interface GetOrdersQuery {
   pageIndex?: number | null;
+  orderId?: string | null;
+  customerName?: string | null;
+  status?: string | null;
 }
 
-export interface GetOrdersResponse{
-    orders: {
-        orderId: string;
-        createdAt: string;
-        status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
-        customerName: string;
-        total: number;
-    }[];
-    meta: {
-        pageIndex: number;
-        perPage: number;
-        totalCount: number;
-    };
+export interface GetOrdersResponse {
+  orders: {
+    orderId: string;
+    createdAt: string;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+  }[];
+  meta: {
+    pageIndex: number;
+    perPage: number;
+    totalCount: number;
+  };
 }
 
-export async function getOrders({pageIndex}: GetOrdersQuery) {
+export async function getOrders({
+  pageIndex,
+  orderId,
+  customerName,
+  status,
+}: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>("/orders", {
     params: {
-        pageIndex,
+      pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   });
 
